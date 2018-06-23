21b7bb602e67f53879d90b7e8981fb9ec5301ec4
Johnny Ramos <johnny.gerardo24@hotmail.com>
2018-06-22 20:10:58 -0500

    complete project

---
+++ TelefonosCRUD/MyTestCase.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelefonosCRUD.Controllers;
+
+namespace TelefonosCRUD
+{
+    // ---------- TEST TELEFONOS ---------- //
+    [TestFixture]
+    public class MyTestCase
+    {
+        [TestCase]
+        public void createTelefono()
+        {
+            TelefonosController tc = new TelefonosController();
+            var telefono = new Models.Telefonos();
+            telefono.Marca = "Samsung";
+            telefono.Modelo = "S8";
+            telefono.Numero = 953991653;
+            telefono.ClienteID = 1;
+
+            ActionResult result = tc.Create(telefono);
+            Assert.That(result, Is.InstanceOf<RedirectToRouteResult>());
+        }
+
+        [TestCase]
+        public void editTelefono()
+        {
+            TelefonosController tc = new TelefonosController();
+            var telefono = new Models.Telefonos();
+            telefono.PhoneID = 3;
+            telefono.Marca = "Apple";
+            telefono.Modelo = "Iphone X";
+            telefono.Numero = 999642138;
+            telefono.ClienteID = 2;
+
+            ActionResult result = tc.Edit(telefono);
+            Assert.That(result, Is.InstanceOf<RedirectToRouteResult>());
+        }
+
+        [TestCase]
+        public void deleteTelefono()
+        {
+            TelefonosController tc = new TelefonosController();
+            var PhoneID = 2;
+
+            ActionResult result = tc.Delete(PhoneID);
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+    }
+}
---
+++ TelefonosCRUD/MyTestCase2.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelefonosCRUD.Controllers;
+
+namespace TelefonosCRUD
+{
+    // ------- TEST CLIENTES ---------- //
+    [TestFixture]
+    public class MyTestCase2
+    {
+        [TestCase]
+        public void createCliente()
+        {
+            ClientesController tc = new ClientesController();
+            var cliente = new Models.Cliente();
+            cliente.Nombre = "Raphael";
+            cliente.ApellidoPaterno = "Bravo";
+            cliente.ApellidoMaterno = "Dedo";
+            cliente.Direccion = "Jr Tacna 298";
+            cliente.DNI = 72721896;
+
+            ActionResult result = tc.Create(cliente);
+            Assert.That(result, Is.InstanceOf<RedirectToRouteResult>());
+        }
+
+        [TestCase]
+        public void editCliente()
+        {
+            ClientesController tc = new ClientesController();
+            var cliente = new Models.Cliente();
+            cliente.ClienteID = 2;
+            cliente.Nombre = "Pieter";
+            cliente.ApellidoPaterno = "Gleixner";
+            cliente.ApellidoMaterno = "Rodrigues";
+            cliente.Direccion = "Donde sea 348";
+            cliente.DNI = 79846532;
+
+            ActionResult result = tc.Edit(cliente);
+            Assert.That(result, Is.InstanceOf<RedirectToRouteResult>());
+        }
+
+        [TestCase]
+        public void deleteCliente()
+        {
+            ClientesController tc = new ClientesController();
+            var ClienteID = 2;
+
+            ActionResult result = tc.Delete(ClienteID);
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+        }
+    }
+}
